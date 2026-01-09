795f7bf50c2a1a80446c71435cc1fce31c37b8be
BagavathiPerumal <bagavathiperumal.a@syncfusion.com>
2026-01-09 19:39:50 +0530

    fix-33351-Made changes to trigger page appearing events before platform navigation to ensure Shell TabBar visibility is applied correctly during multi-level back navigation.

--- src/Controls/src/Core/Shell/ShellSection.cs
+++ src/Controls/src/Core/Shell/ShellSection.cs
@@ -844,9 +844,11 @@ namespace Microsoft.Maui.Controls
 				RequestType = NavigationRequestType.PopToRoot
 			};
 
-			InvokeNavigationRequest(args);
 			var oldStack = _navStack;
 			_navStack = new List<Page> { null };
+
+			PresentedPageAppearing();
+			InvokeNavigationRequest(args);
 
 			if (args.Task != null)
 				await args.Task;
@@ -859,8 +861,6 @@ namespace Microsoft.Maui.Controls
 				oldStack[i].SendDisappearing();
 				RemovePage(oldStack[i]);
 			}
-
-			PresentedPageAppearing();
 		}
 
 		protected virtual Task OnPushAsync(Page page, bool animated)
